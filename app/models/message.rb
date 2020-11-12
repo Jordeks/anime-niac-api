0dae5e17dc7875162db243b74d4b9d7d16ad3eeb
jordles113 <jordan.t.romero@gmail.com>
2020-11-12 15:15:43 -0600

    working on message serializer

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,12 @@
 class Message < ApplicationRecord
   belongs_to :user
+
+  def username
+    user.username
+  end
+
+  def username=(username)
+    self.user = User.find_or_create_by(username: username)
+  end
+  
 end
--- app/services/message_serializer.rb
+++ app/services/message_serializer.rb
@@ -1,12 +1,13 @@
 class MessageSerializer
 
     def initialize(message_object)
-      @message = message_object
+      @messages = message_object
     end
 
     def to_serialized_json
-      @message.to_json(:except => [:created_at, :updated_at],
-      :include => {:user => {:only => :username}})
+      byebug
+      # @message.to_json(:include => [:id, :text, :username])
+      @messages.to_json(:except => [:created_at, :updated_at], :include => {:user => {:only => :username}})
     end
 
   end
